93a0794649d8c7b22d99b76eae63f3405b99be8e
edilayne-ramos <edilayne.silva1@sou.ufmt.com.br>
2025-10-08 18:15:27 -0300

    programacao3

---
+++ aula7/file_nome.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BufferedReader reader = null;
+
+        try {
+            System.out.print("Digite o nome do arquivo de texto: ");
+            String nomeArquivo = scanner.nextLine();
+
+            FileInputStream fileInputStream = new FileInputStream(nomeArquivo);
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+            reader = new BufferedReader(inputStreamReader);
+
+            String linha;
+            System.out.println("\nConteúdo do arquivo:");
+            while ((linha = reader.readLine()) != null) {
+                System.out.println(linha);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Erro.");
+        } catch (IOException e) {
+            System.out.println("Erro ao ler o arquivo.");
+        } finally {
+            // Fechando os streams
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+                scanner.close();
+            } catch (IOException e) {
+                System.out.println("Erro ao fechar o stream.");
+            }
+        }
+    }
+}
